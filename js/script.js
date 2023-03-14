5d2b4eef11c0849816365487f93cf80732dadfab
Dmitry <dmitrynikotin@mail.ru>
2023-03-14 17:32:32 +0300

    upd

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 'use strict';
 
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
+
 import  tabs from './modules/tabs';
-        import modal from './modules/modal';
-        import timer from './modules/timer';
-        import cards from './modules/cards';
-        import calc from './modules/calc';
-        import forms from './modules/forms';
-        import slider from './modules/slider';
-        import {openModal} from './modules/modal';
+import modal from './modules/modal';
+import timer from './modules/timer';
+import cards from './modules/cards';
+import calc from './modules/calc';
+import forms from './modules/forms';
+import slider from './modules/slider';
+import {openModal} from './modules/modal';
 
 document.addEventListener('DOMContentLoaded', () => {
     const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 500000);
